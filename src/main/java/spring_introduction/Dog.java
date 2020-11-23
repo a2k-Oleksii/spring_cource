957902575e2d194c11f44cfbfd617f4767e78685
a2k-Oleksii <a2k_oleksii@ukr.net>
2020-11-23 13:28:09 +0200

    First commit

---
+++ src/main/java/spring_introduction/Dog.java
@@ -0,0 +1,13 @@
+package spring_introduction;
+
+public class Dog implements Pet {
+
+    public Dog() {
+        System.out.println("Dog bean is created!");
+    }
+
+    @Override
+    public void say() {
+        System.out.println("Gav-Gav!");
+    }
+}
