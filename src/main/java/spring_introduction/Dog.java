8f44c41c7ae422a01f2c593617fe55ea492693ae
a2k-Oleksii <a2k_oleksii@ukr.net>
2020-11-23 17:36:27 +0200

    Create new test file Test4.java, new file applicationContext3Prototype.xml. Change file application Cpntext2.xml and Dog.java (add fild name and her Set method ang Get method)

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,11 +1,20 @@
 package spring_introduction;
 
 public class Dog implements Pet {
+    private String name;
 
     public Dog() {
         System.out.println("Dog bean is created!");
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     @Override
     public void say() {
         System.out.println("Gav-Gav!");
