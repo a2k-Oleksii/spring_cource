c8a7d1f2acbb49dbdd0978fb26db7708ba2d2490
a2k Oleksii St <a2k_oleksii@ukr.net>
2020-11-29 12:14:12 +0200

    add anotation @Autowired

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -1,19 +1,28 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("personBean") 
 public class Person {
 
+    @Autowired
     private Pet pet;
     private String surname;
     private int age;
 
     public Person() {
-    }
-
-    public Person(Pet pet) {
         System.out.println("Person bean is created!");
-        this.pet = pet;
     }
 
+//    @Autowired
+//    public Person(Pet pet) {
+//        System.out.println("Person bean is created!");
+//        this.pet = pet;
+//    }
+
+    
+//    @Autowired
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet!");
         this.pet = pet;
