8cfd40322a3283080eed8ec0e4a11eea7bc089b9
a2k Oleksii St <a2k_oleksii@ukr.net>
2020-11-29 16:59:41 +0200

    compiling files in package target

---
+++ src/main/java/spring_introduction/ScopeTest.java
@@ -0,0 +1,23 @@
+package spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ScopeTest {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
+        
+        Dog myDog = context.getBean("dogBean", Dog.class);
+        Dog yourDog = context.getBean("dogBean", Dog.class);
+        
+        myDog.say();
+        
+        System.out.println(myDog == yourDog);
+        
+        System.out.println(myDog);
+        System.out.println(yourDog);
+        
+        context.close();
+    }
+    
+}
