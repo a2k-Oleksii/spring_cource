288dd926195bf5f158c1a112e6f71638a723cd3b
a2k Oleksii St <a2k_oleksii@ukr.net>
2020-11-29 16:54:28 +0200

    add anotations @PostConstruct and @PreConstruct

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,8 +1,12 @@
 package spring_introduction;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("dogBean")
+//@Scope("prototype")
 public class Dog implements Pet {
 //    private String name;
 
@@ -10,10 +14,12 @@ public class Dog implements Pet {
         System.out.println("Dog bean is created!");
     }
     
+    @PostConstruct
     public void init(){
         System.out.println("Class Dog init method.");
     }
     
+    @PreDestroy
     public void destroy() {
         System.out.println("Class Dog destroy method.");
     }
