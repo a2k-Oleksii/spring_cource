f4ea4cd359f8f2b71e386c2b450a94671eb471e7
a2k Oleksii St <a2k_oleksii@ukr.net>
2020-11-29 12:15:39 +0200

    test classes with anotation @Autowired

--- src/main/java/spring_introduction/ConfigWithAnotation1.java
+++ src/main/java/spring_introduction/ConfigWithAnotation1.java
@@ -7,9 +7,13 @@ public class ConfigWithAnotation1 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
         
-        Cat myCat = context.getBean("catBean", Cat.class);
         
-        myCat.say();
+        Person person = context.getBean("personBean", Person.class);
+        person.callYourPet();
+        
+//        Cat myCat = context.getBean("catBean", Cat.class);
+//        
+//        myCat.say();
         
         context.close();
     }
