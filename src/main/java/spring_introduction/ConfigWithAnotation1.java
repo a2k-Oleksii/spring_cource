5dfe5f48fb9cddd62bf2e31e48d160834c62a53d
a2k Oleksii St <a2k_oleksii@ukr.net>
2020-11-29 12:57:41 +0200

    use
    methods getSurname and getAge from tetsting class Person.java

--- src/main/java/spring_introduction/ConfigWithAnotation1.java
+++ src/main/java/spring_introduction/ConfigWithAnotation1.java
@@ -6,15 +6,16 @@ public class ConfigWithAnotation1 {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext3.xml");
-        
-        
+
         Person person = context.getBean("personBean", Person.class);
         person.callYourPet();
-        
+
 //        Cat myCat = context.getBean("catBean", Cat.class);
 //        
 //        myCat.say();
-        
+        System.out.println("Person surname: " + person.getSurname());
+        System.out.println("Person age: " + person.getAge());
+
         context.close();
     }
 }
