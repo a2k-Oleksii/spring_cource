239b317195640afa3daa935f13d238bc234c19da
a2k Oleksii St <a2k_oleksii@ukr.net>
2020-11-29 17:27:25 +0200

    coment anotation @Component

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -5,7 +5,7 @@ import javax.annotation.PreDestroy;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-@Component("dogBean")
+//@Component("dogBean")
 //@Scope("prototype")
 public class Dog implements Pet {
 //    private String name;
--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component("personBean")
+//@Component("personBean")
 public class Person {
 
 //    @Autowired
@@ -21,8 +21,9 @@ public class Person {
         System.out.println("Person bean is created!");
     }
 
-    @Autowired
-    public Person(@Qualifier("catBean") Pet pet) {
+//    @Autowired
+    public Person(//@Qualifier("catBean") 
+            Pet pet) {
         System.out.println("Person bean is created!");
         this.pet = pet;
     }
